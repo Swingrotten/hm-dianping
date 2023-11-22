0dc0298c3c81a92f946a6f6f5b8862b36c7a2813
Cao Changzhuo <2126026553@qq.com>
2023-11-22 12:00:52 +0800

    添加商铺列表缓存

--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TYPE_KEY;
 
 /**
  * <p>
@@ -17,4 +26,37 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
 
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    public ShopTypeServiceImpl(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    @Override
+    public Result queryTypeList() {
+        //1. 从redis查询商铺缓存
+        String shopTypeJson = stringRedisTemplate.opsForValue().get(CACHE_SHOP_TYPE_KEY);
+
+        //2. 判断是否存在
+        if(StrUtil.isNotBlank(shopTypeJson)){
+            //3. 如果存在,则返回
+            List<ShopType> shopTypes = JSONUtil.toList(shopTypeJson, ShopType.class);
+            return Result.ok(shopTypes);
+        }
+
+        //4. 不存在,根据id查询数据库
+        List<ShopType> typeList = query().orderByAsc("sort").list();
+
+        //5. 不存在,返回错误
+        if(typeList.toString()==null){
+            Result.fail("店铺不存在! ");
+        }
+
+        //6. 存在,写入redis
+        stringRedisTemplate.opsForValue().set(CACHE_SHOP_TYPE_KEY,JSONUtil.toJsonStr(typeList));
+
+        //7. 返回
+        return Result.ok(typeList);
+    }
 }
