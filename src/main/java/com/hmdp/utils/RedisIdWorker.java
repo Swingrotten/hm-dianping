c95acf4b7468ff82dcb1b737b54cfc8a3785c40b
Cao Changzhuo <2126026553@qq.com>
2023-11-29 21:46:26 +0800

    实现秒杀下单

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -32,7 +32,7 @@ public class RedisIdWorker {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    private long nextId(String keyPrefix){
+    public long nextId(String keyPrefix){
         //1. 生成时间戳
         LocalDateTime now = LocalDateTime.now();
         long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
